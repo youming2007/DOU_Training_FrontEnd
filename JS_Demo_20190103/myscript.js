4e6e0cbe229b4fb85f89963d8deec4c62633ec61
SonHuynh <sonitiuh@gmail.com>
2019-01-03 20:12:23 +0700

    Tuan 02 JavaScript

---
+++ JS_Demo_20190103/myscript.js
@@ -0,0 +1,21 @@
+var timer = null;
+function keyDown() {
+    document.getElementById("content").innerHTML = "Wating for you to stop typing...";
+    clearTimeout(timer);
+    timer = setTimeout(stopTyping, 500)
+};
+
+function stopTyping() {
+    //doing
+    let yourname = document.getElementById("textInput").value;
+    let endChar = yourname.substr(-1);
+    endChar == '?' ? callApis() : document.getElementById("content").innerHTML = "Question usually contain a question mark ;-)";
+}
+async function callApis() {
+    let response = await fetch('https://yesno.wtf/api');
+    let dataResult = await response.json();
+    dataResult.answer == 'yes' ? document.getElementById("content").innerHTML = 'Yes' :
+        document.getElementById("content").innerHTML = 'Thinking...'
+
+
+}
