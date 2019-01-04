02466eeb7d94114c314096003246b7b5eec8007c
ThieuLe <levanthieu@gmail.com>
2019-01-05 00:20:20 +0700

    JS_Demo_20190103

--- JS_Demo_20190103/js/script.js
+++ JS_Demo_20190103/js/script.js
@@ -1,34 +1,34 @@
 var timeout;
 var result = document.getElementById("txtResult");
 var input = document.getElementById("txtInput");
-function delayTyping(){
-    timeout = 0;
-    input.onkeydown = function(){
-        timeout++;
-        setTimeout("lookup("+timeout+")",500);
-    }
+
+
+input.onkeydown = function(){  result.innerHTML = "Waiting for you to stop typing..";
 }
 
 
-window.onload = delayTyping;
-function lookup(key_down){
-    if(key_down == timeout){
-        const url = 'https://yesno.wtf/api';
-        var last = input.value;
-        if(last.slice(-1) === "?"){
-            fetch(url)
-                .then(response => response.json())
-                .then((data) => {
-                    if(data.answer === "yes"){
-                        result.innerHTML = "Yes.";
-                    }else{
-                        result.innerHTML = "Thinking...";
-                    }
-            }).catch(err=> console.error(error));   
-        }else{
-          result.innerHTML = "Question usually contain a question mark...";
-        }
-    }else{
-        result.innerHTML = "Waiting for you to stop typing..."
-    }
+function lookup(){
+        timeout = null;
+        input.onkeyup = function(){
+          
+          clearTimeout(timeout);
+          timeout = setTimeout(function(){
+            const url = 'https://yesno.wtf/api';
+            var last = input.value;
+            if(last.slice(-1) === "?"){
+                fetch(url)
+                    .then(response => response.json())
+                    .then((data) => {
+                        if(data.answer === "yes"){
+                            result.innerHTML = "Yes.";
+                        }else{
+                            result.innerHTML = "Thinking...";
+                        }
+                }).catch(err=> console.error(error));   
+            }else{
+              result.innerHTML = "Question usually contain a question mark...";
+            }
+        },500);
+  }
 }
+window.onload = lookup;
